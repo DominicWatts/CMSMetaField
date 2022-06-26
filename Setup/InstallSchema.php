effd681089c8f14bf804c826c01384c251469a6c
Dominic Watts <dominic@pixie.agency>
2022-06-27 00:04:21 +0100

    Fixes

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -8,7 +8,7 @@ use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 
 /**
- * Class InstallSchema
+ * Class InstallSchema to add custom field CMS field
  */
 class InstallSchema implements InstallSchemaInterface
 {
